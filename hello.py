01d622b746dc41a38861fa700335f4a6adcdb0c1
JEEVA SURIYS S <jeevasuriya2001@gmail.com>
2024-07-15 20:09:14 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,8 @@
+#import flask
+from flask import Flask
+app=Flask(__name__)
+@app.route('/')
+def hello_world():
+    return "<h1>hello_world</h1></div><br><h2>HELLO APPA YANGA PA IRUKENGA<h2>"
+if __name__=='__main__':
+    app.run(debug=True)
