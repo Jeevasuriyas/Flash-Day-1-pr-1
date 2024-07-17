4a3e045936b8dd33db1098127d79c5d944b634d3
JEEVA SURIYS S <jeevasuriya2001@gmail.com>
2024-07-17 21:41:47 +0530

    hello.py

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def hello_world():
-    return "<h1>hello_world</h1></div><br><h2>HELLO APPA YANGA PA IRUKENGA<h2>"
+    return "<h1>hello_world</h1></div><br><h2>HELLO Jeevasuriya!!!<h2>"
 if __name__=='__main__':
     app.run(debug=True)
